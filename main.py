c5224cc7d58907140e90a51d24c4d630bbbcc02a
Ozone <70636940+LukyGithub@users.noreply.github.com>
2022-09-28 10:22:46 +0200

    tests with kivy

--- main.py
+++ main.py
@@ -1,100 +1,35 @@
-import sys
-import pygame as pg
-from sprites import *
+from kivy.app import App
+from kivy.properties import NumericProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.button import Button
+from kivy.metrics import dp
 
-screen_size = (800, 600)
-aspect_ratio = screen_size[0]/screen_size[1]
+class WidgetsTest(BoxLayout):
+    count = NumericProperty(0)
+    def button_click(self):
+        self.count += 1
 
-def scrPos(x, y):
-    return (x, screen_size[1] - y)
+class StackLayoutTest(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b = []
+        for i in range(0, 100):
+            b.append(Button(text=str(i+1), size_hint=(None, None), width=dp(100), height=dp(100)))
+            self.add_widget(b[i])
 
-def unit(x, y):
-    return(x * screen_size[0] / (12 * aspect_ratio), screen_size[1] + y / screen_size[1] * 12)    
+# class GridLayoutTest(GridLayout):
+#     pass
 
-def main():
-    pg.init()
+class AnchorLayoutTest(AnchorLayout):
+    pass
 
-    pg.display.set_caption("Epicka Hra")
+class MainWidget(BoxLayout):
+    pass
 
-    screen = pg.display.set_mode(screen_size, pg.NOFRAME)
-
-    clock = pg.time.Clock()
-
-    #active sprite group
-    activeGroup = pg.sprite.Group()
-
-    chad = Sprite("chad.png", )
-    direction = [0, 0]
-
-    #movement registerers
-    player = {
-        "left": False,
-        "right": False,
-        "jumpHeight": -10,
-        "movementSpeed": 0.5
-    }
-
-    #player statuses initialization
-    verticalVelocity = 1
-    gravity = 0.36
-    grounded = False
-
-    chad.add(activeGroup)
-
-    staticGroupFront = pg.sprite.Group()
-
-    map1 = [Block("boatFence.png", 100, 100, scrPos(0,0))]
-
-    for i in range(0, len(map1)):
-        map1[i].add(staticGroupFront)
-
-    running = True
-    while(running):
-        #Player statuses:
-        if(chad.get_position()[1] > scrPos(0, 125)[1]):
-            grounded = True
-            verticalVelocity = 0
-        else:
-            verticalVelocity += gravity
-            verticalVelocity = min(verticalVelocity, 15)
-            grounded = False
-        
-        for event in pg.event.get():
-            if (event.type == pg.QUIT):
-                running = False
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_d:
-                    player["right"] = True
-                if event.key == pg.K_a:
-                    player["left"] = True
-                if event.key == pg.K_SPACE:
-                    if grounded:
-                        verticalVelocity = player["jumpHeight"]
-
-            if event.type == pg.KEYUP:
-                if event.key == pg.K_d:
-                    player["right"] = False
-                if event.key == pg.K_a:
-                    player["left"] = False
-
-        screen.fill((83, 92, 104))
-
-        deltaTime = clock.get_time()
-
-        direction[0] = (1 * player["right"] - 1 * player["left"]) * player["movementSpeed"] * deltaTime
-
-        direction[1] = verticalVelocity
-
-        activeGroup.update(direction)
-        activeGroup.draw(screen)
-
-        staticGroupFront.draw(screen)
-
-        clock.tick(60)
-        pg.display.flip()
-
-    pg.quit()
-    sys.exit()
+class MainApp(App):
+    pass
 
 if __name__ == "__main__":
-    main()
+    MainApp().run()
